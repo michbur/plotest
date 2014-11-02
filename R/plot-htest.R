2d880277adfb0bd18e5b608fd587137afdb6dc30
michbur <michalburdukiewicz@gmail.com>
2014-11-02 15:16:20 +0100

    changes in documentation

--- R/plot-htest.R
+++ R/plot-htest.R
@@ -2,21 +2,23 @@
 #'
 #' Plots htest objects.
 #'
-#' This is a generic function: methods can be defined for it directly
-#' or via the \code{\link{Summary}} group generic. For this to work properly,
-#' the arguments \code{...} should be unnamed, and dispatch is on the
-#' first argument.
+#' @param x an object of class \code{htest}.
+#' @param legend_pos a position of the text on the plot. Either matrix or a single keyword.
+#' See Details section of \code{\link[graphics]{legend}}.
+#' @param digits number of digits.
+#' @param ... additional arguments sent to \code{\link[graphics]{legend}}. 
 #' @examples
 #' x <- rnorm(10)
 #' y <- rnorm(10)
 #' sample_test <- cor.test(x, y)
 #' plot(x, y)
-#' plot(sample_test, topleft)
+#' plot(sample_test, "topleft")
 #' 
 
 plot.htest <- function(x, legend_pos = "topleft", 
-                       digits = 4L, quote = TRUE, prefix = "", ...) {
+                       digits = 4L, ...) {
   #output
-  out <- capture.output(print(x, digits = digits, quote = quote, prefix = "", ...))
-  legend(x = legend_pos, legend = out)
+  out <- capture.output(print(x, digits = digits))
+  browser()
+  legend(x = legend_pos, legend = out, ...)
 }
