4418d2516e23e050d5307e8e5bd10fc4a682c813
michbur <michalburdukiewicz@gmail.com>
2014-11-03 22:54:48 +0100

    minor refinements to plot.htest

--- R/plot-htest.R
+++ R/plot-htest.R
@@ -1,24 +1,25 @@
 #' Plots htest objects
 #'
-#' Plots htest objects.
+#' Adds htest objects to existing plots.
 #'
 #' @param x an object of class \code{htest}.
 #' @param legend_pos a position of the text on the plot. Either matrix or a single keyword.
 #' See Details section of \code{\link[graphics]{legend}}.
 #' @param digits number of digits.
+#' @param bty the type of box to be drawn around the plotted test. The allowed values are "o" 
+#' (border around plotted test information) and "n" (no border; the default).
 #' @param ... additional arguments sent to \code{\link[graphics]{legend}}. 
 #' @examples
-#' x <- rnorm(10)
-#' y <- rnorm(10)
+#' x <- 1:20
+#' y <- exp(x)
 #' sample_test <- cor.test(x, y)
 #' plot(x, y)
 #' plot(sample_test, "topleft")
 #' 
 
 plot.htest <- function(x, legend_pos = "topleft", 
-                       digits = 4L, ...) {
+                       digits = 4L, bty = "n", ...) {
   #output
   out <- capture.output(print(x, digits = digits))
-  browser()
-  legend(x = legend_pos, legend = out, ...)
+  legend(x = legend_pos, legend = out, bty = bty, ...)
 }
