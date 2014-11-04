ea5d7300f9699ce26df6b0b2b34b5c9f97a3bb50
michbur <michalburdukiewicz@gmail.com>
2014-11-04 16:07:45 +0100

    plot.htest with line breaking

--- R/plot-htest.R
+++ R/plot-htest.R
@@ -8,18 +8,33 @@
 #' @param digits number of digits.
 #' @param bty the type of box to be drawn around the plotted test. The allowed values are "o" 
 #' (border around plotted test information) and "n" (no border; the default).
+#' @param n_char \code{integer} representing maximum number of character in single line
+#' of the output.
+#' @param extend a non-negative integer specifying the indentation of subsequent lines in 
+#' paragraphs.
 #' @param ... additional arguments sent to \code{\link[graphics]{legend}}. 
+#' @details Longer lines are splitted on whitespaces using 
+#' \code{\link[base]{strwrap}} function.
 #' @examples
+#' 
+#' #Example 1: test for correlation between paired samples
 #' x <- 1:20
 #' y <- exp(x)
-#' sample_test <- cor.test(x, y)
-#' plot(x, y)
-#' plot(sample_test, "topleft")
+#' sample_cortest <- cor.test(x, y)
+#' plot(x, y, type = "b")
+#' plot(sample_cortest, "topleft")
 #' 
+#' #Example 2: Pearson's Chi-squared Test for Count Data
+#' x <- matrix(c(12, 5, 7, 7), ncol = 2)
+#' sample_chisqtest <- chisq.test(x, y)
+#' barplot(x, beside = TRUE)
+#' plot(sample_chisqtest, "topright")
 
-plot.htest <- function(x, legend_pos = "topleft", 
-                       digits = 4L, bty = "n", ...) {
+plot.htest <- function(x, legend_pos = "topleft", digits = 4L, bty = "n", n_char = 40,
+                       exdent = 2, ...) {
   #output
   out <- capture.output(print(x, digits = digits))
+  n_chars <- sapply(out, nchar, USE.NAMES = FALSE)
+  out <- strwrap(out, n_char, exdent = exdent)
   legend(x = legend_pos, legend = out, bty = bty, ...)
 }
